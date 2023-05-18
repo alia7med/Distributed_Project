27e1d0d217e78024c407241c6b50c2c74bf390d7
abnabdelmonem <ahmed.abn.abdelmonem@gmail.com>
2023-05-18 14:08:17 +0300

    add script to run many clients in parallel

---
+++ src/main/java/org/example/graph/RunClients.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the number of instances from the first argument, or use 5 if not provided
+num_instances=${1:-5}
+
+# Define the base command to run the Java program
+java_command="java -jar graph_client.jar"
+
+# Loop through the number of instances and run the Java program in the background
+# shellcheck disable=SC2004
+for ((i=0; i<$num_instances; i++))
+do
+  # Append the i value to the Java command
+  command_with_i="$java_command $i"
+  $command_with_i &
+done
+
+# Wait for all instances to finish
+wait
